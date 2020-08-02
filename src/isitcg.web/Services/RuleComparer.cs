92526b52eaa64109423678e66d9454bd6bfc6d36
Lisa Gardner <lisagardner@outlook.com>
2020-08-02 17:22:27 -0400

    Only use rank number

--- src/isitcg.web/Services/RuleComparer.cs
+++ src/isitcg.web/Services/RuleComparer.cs
@@ -8,18 +8,8 @@ namespace isitcg
     {
         public int Compare(Rule x, Rule y)
         {
-            if (x.Result == y.Result){
-                var rank = x.Rank.CompareTo(y.Rank);
-                return rank == 0 ? 1 : rank;
-            }
-
-            if (x.Result == "danger")
-                return -1;
-            if (y.Result == "danger")
-                return 1;
-            if (x.Result == "warning")
-                return -1;
-            return 1;
+            var rank = x.Rank.CompareTo(y.Rank);
+            return rank == 0 ? 1 : rank;
         }
     }
 }

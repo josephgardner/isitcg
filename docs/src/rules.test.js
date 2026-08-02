6db28e85d37218f5e519822a163fb4570f850c54
Joseph Gardner <josephgardner@outlook.com>
2026-08-02 14:20:43 -0400

    Fix ingredient rules deployment test

--- docs/src/rules.test.js
+++ docs/src/rules.test.js
@@ -5,18 +5,11 @@ import { isValidRulesData } from './isitcg.js'
 const rootRules = JSON.parse(
   await readFile(new URL('../../ingredientrules.json', import.meta.url), 'utf8'),
 )
-const pagesRules = JSON.parse(
-  await readFile(new URL('../ingredientrules.json', import.meta.url), 'utf8'),
-)
 
-describe('ingredient rules deployment data', () => {
+describe('ingredient rules data', () => {
   test('the source-of-truth rules have the expected shape', () => {
     expect(isValidRulesData(rootRules)).toBe(true)
     expect(new Set(rootRules.Rules.map(rule => rule.Name)).size)
       .toBe(rootRules.Rules.length)
-  })
-
-  test('the GitHub Pages rules match the source of truth', () => {
-    expect(pagesRules).toEqual(rootRules)
   })
 })
